c36b08fbda0403117db7c3ad8835541803f78abc
Kasper Janssens <kasper.janssens@hgst.com>
2016-09-29 11:51:01 +0200

    Trying to remove duplicates

--- lib/plant_collector.ml
+++ lib/plant_collector.ml
@@ -28,52 +28,33 @@ let main =
     ) puml_files
   in
   let () = Printf.eprintf "Creating plant map\n" in
-  let plant_map = List.fold_left Plant_model.to_hash BatMap.empty plants in
+  let structure_item_to_function_calls_map = List.fold_left Plant_model.to_hash BatMap.empty plants in
+
   let () = Printf.eprintf "Creating function calls\n" in
-  let originating_function_calls = BatList.map Plant_model.originating_function_call plants in
-  let nr_of_calls= BatList.length originating_function_calls in
+  let function_calls_that_are_structure_items = BatList.map Plant_model.originating_function_call plants in
+
+  let nr_of_calls= BatList.length function_calls_that_are_structure_items in
   let () = Printf.eprintf "Creating function to tree map, over %d function calls\n" nr_of_calls in
   let (_, function_to_tree_map) = BatList.fold_left (fun (i, map) function_call ->
-      let subtree = Plant_tree.create_subtree plant_map function_call in
-(*      let () = Printf.eprintf "Created subtree %d of %d\n" i nr_of_calls in
-      let () = Printf.eprintf "Function Call : %s\n" @@ Plant_model.function_call_to_string function_call in*)
+      let subtree = Plant_tree.create_subtree structure_item_to_function_calls_map function_call in
       (i+1, BatMap.add function_call subtree map)
-    ) (1, BatMap.empty) (BatList.rev originating_function_calls)
+    ) (1, BatMap.empty) (BatList.rev function_calls_that_are_structure_items)
   in
   let () = Printf.eprintf "creating functions used by other functions\n" in
-  let values = BatMap.values function_to_tree_map in
-  let values_length = BatList.length @@ BatList.of_enum values in
-  let () = Printf.eprintf "value length %d\n" values_length in
-  let (_, functions_used_by_other_functions) = BatEnum.fold (fun (i,acc) elem ->
-      let () = Printf.eprintf "Function used by other functions: %d of %d\n" i values_length  in
-      let used_functions_opt = Plant_tree.flatten_and_tail elem in
-      match used_functions_opt with
-      | None -> (i+1,acc)
-      | Some functions -> (i+1, BatList.append acc functions)
-    ) (1, []) values
-  in
-  let functions_used_by_other_functions = BatList.unique functions_used_by_other_functions in
-  let () = Printf.eprintf "Creating all trees\n" in
 
   let size_of_map = BatList.length @@ BatList.of_enum @@ BatMap.keys function_to_tree_map in
-  let () = Printf.eprintf "Size of map is %d\n" size_of_map in
-  let test_tree = Plant_tree.Node(1,[Plant_tree.Leaf 2;Plant_tree.Leaf 3]) in
-  let _ = Plant_tree.fold (fun acc elem -> acc + elem) 0 test_tree in
   let _ =
     BatMap.foldi (fun key value i ->
-      if BatList.mem key functions_used_by_other_functions then
-        (i+1)
-      else
-        let file_name = Printf.sprintf "%s/%s.puml" "pummels" @@ Plant_model.function_call_to_string key in
-        let () = BatFile.with_file_out ~mode:[`create] file_name (
-          fun output ->
-            let () = BatIO.write_line output "@startuml" in
-            let (_, output) = Plant_tree.tree_to_string "Actor" output value in
-            BatIO.write_line output "@enduml"
-        )
-        in
-        (i+1)
+      let file_name = Printf.sprintf "%s/%s.puml" "pummels" @@ Plant_model.function_call_to_string key in
+      let () = BatFile.with_file_out ~mode:[`create] file_name (
+        fun output ->
+          let () = BatIO.write_line output "@startuml" in
+          let (_, output) = Plant_tree.tree_to_string "Actor" output value in
+          BatIO.write_line output "@enduml"
       )
-      function_to_tree_map (1)
+      in
+      (i+1)
+    )
+    function_to_tree_map (1)
   in
   Printf.eprintf "Collection done\n"
